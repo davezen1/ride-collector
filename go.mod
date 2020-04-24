58eeef4a46bc0bd4610c3b6a28b00a602037987e
David Hodge <david.hodge@lucidtechnics.com>
2020-04-24 10:43:20 -0400

    feat: initial testing of writing to csv
    - not sure how best to write structs to csv, but multidimensional slices are straightforward
    - may introduce interface and config for extracting info from multiple sites
    - made all struct data strings
    - upgrade to collyv2, somehow it was downgraded after go mod?

--- go.mod
+++ go.mod
@@ -6,13 +6,15 @@ require (
 	github.com/PuerkitoBio/goquery v1.5.1 // indirect
 	github.com/antchfx/htmlquery v1.2.2 // indirect
 	github.com/antchfx/xmlquery v1.2.3 // indirect
-	github.com/antchfx/xpath v1.1.5 // indirect
-	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly v1.2.0
+	github.com/antchfx/xpath v1.1.6 // indirect
+	github.com/gocolly/colly/v2 v2.0.1
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
-	github.com/temoto/robotstxt v1.1.1 // indirect
-	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
-	google.golang.org/appengine v1.6.5 // indirect
+	github.com/golang/protobuf v1.4.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/net v0.0.0-20200421231249-e086a090c8fd // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- main.go
+++ main.go
@@ -1,22 +1,61 @@
 package main
 
 import (
+	"encoding/csv"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 	"strings"
 
-	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/v2"
 )
 
-type ride struct {
-	name                           string
-	shortDescription               string
-	imageURL                       string
-	longDescription                string
-	tags                           []string
-	heightRequirementInInches      int
-	heightRequirementInCentimeters int
-	rideURL                        string
+//Ride details
+type Ride struct {
+	name, shortDescription, imageURL, heightRequirementInInches, rideURLstring, tags string
+}
+
+//Rides represents slice of Ride structs
+type Rides []Ride
+
+func buschgardens(c colly.Collector) ([]Ride, [][]string) {
+	var ridesList Rides
+	var ridesSlices = [][]string{{"name", "image", "description", "tags", "height"}}
+
+	c.OnHTML("#page-content > div > div > ul", func(e *colly.HTMLElement) {
+
+		e.ForEach("li", func(_ int, el *colly.HTMLElement) {
+
+			image := e.Request.URL.String() + el.ChildAttr("span > a > img", "src")
+			name := el.ChildText("div > h2 > a")
+			shortDescription := el.ChildText("div > p")
+			tags := strings.Join(el.ChildTexts("div > ul > li"), ",")
+
+			if name != "" {
+				//follow link
+				rideURL := e.Request.URL.Scheme + "://" + e.Request.URL.Host + e.Request.URL.Opaque + el.ChildAttr("span > a", "href")
+				c2 := colly.NewCollector()
+				c2.OnHTML("#page-content", func(e2 *colly.HTMLElement) {
+					height := e2.ChildText("#page-content > div.container > div > div.col-sm-8 > div > ul > li:nth-child(1) > dl > dd")
+					height = strings.TrimSuffix(height, "\"")
+
+					ridesList = append(ridesList, Ride{name: name, imageURL: image, shortDescription: shortDescription, tags: tags, heightRequirementInInches: height})
+
+					ridesSlices = append(ridesSlices, []string{name, image, shortDescription, tags, height})
+				})
+
+				c2.Visit(rideURL)
+
+			}
+
+		})
+
+	})
+	c.Visit("https://buschgardens.com/williamsburg/rides")
+
+	return ridesList, ridesSlices
 }
 
 func main() {
@@ -30,42 +69,40 @@ func main() {
 		log.Println("Something went wrong:", err)
 	})
 
-	//make generic with interface to collect ride info (scrape) and write
-	buschgardens(*c)
+	//make generic with interface to collect ride info (extract) and write
+	bgRidesList, bgRidesSlices := buschgardens(*c)
+
+	write(bgRidesList, bgRidesSlices)
+}
+
+//experimenting with multidimensional slices and structs writing to csv
+func write(r Rides, rs [][]string) {
+	log.Println(r)
+	ridesJSON, err := json.Marshal(r)
+	if err != nil {
+		log.Fatal("Cannot encode to JSON ", err)
+	}
+	// file, _ := json.MarshalIndent(r, "", " ")
+
+	_ = ioutil.WriteFile("bg.json", ridesJSON, 0644)
+
+	//write using [][]string
+	file, err := os.Create("bgslice.csv")
+	checkError("Cannot create file", err)
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, value := range rs {
+		err := writer.Write(value)
+		checkError("Cannot write to file", err)
+	}
 
 }
 
-func buschgardens(c colly.Collector) {
-	c.OnHTML("#page-content > div > div > ul", func(e *colly.HTMLElement) {
-
-		e.ForEach("li", func(_ int, el *colly.HTMLElement) {
-
-			image := e.Request.URL.String() + el.ChildAttr("span > a > img", "src")
-			name := el.ChildText("div > h2 > a")
-			shortDescription := el.ChildText("div > p")
-			tags := el.ChildText("div > ul > li")
-
-			if name != "" {
-				log.Printf("name %v sdesc %v \n", name, shortDescription)
-				log.Printf("image %v \n", image)
-				log.Printf("tags %v \n", tags)
-
-				//follow link
-				rideURL := e.Request.URL.Scheme + "://" + e.Request.URL.Host + e.Request.URL.Opaque + el.ChildAttr("span > a", "href")
-				log.Printf("rideURL %v \n", rideURL)
-				c2 := colly.NewCollector()
-				c2.OnHTML("#page-content", func(e2 *colly.HTMLElement) {
-					height := e2.ChildText("#page-content > div.container > div > div.col-sm-8 > div > ul > li:nth-child(1) > dl > dd")
-					height = strings.TrimSuffix(height, "\"")
-					log.Printf("height %v \n", height)
-				})
-
-				c2.Visit(rideURL)
-
-			}
-
-		})
-
-	})
-	c.Visit("https://buschgardens.com/williamsburg/rides")
+func checkError(message string, err error) {
+	if err != nil {
+		log.Fatal(message, err)
+	}
 }
